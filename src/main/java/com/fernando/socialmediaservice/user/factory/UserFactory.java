5f76cd8e7ef0e33cc7c89b77ed6f8f91eaa9969c
frohr <frohr@grupodimed.com.br>
2021-11-28 17:38:29 -0300

    Corrige bug de atualização de usuário

--- src/main/java/com/fernando/socialmediaservice/user/factory/UserFactory.java
+++ src/main/java/com/fernando/socialmediaservice/user/factory/UserFactory.java
@@ -11,12 +11,16 @@ public class UserFactory {
     public static UserModel updateUser(UserModel newUser, UserModel oldUser) {
         return UserModel.builder()
                 .id(oldUser.getId())
+                .name(Optional.ofNullable(newUser)
+                        .map(UserModel::getName)
+                        .orElse(oldUser.getName()))
                 .email(Optional.ofNullable(newUser)
                         .map(UserModel::getEmail)
                         .orElse(oldUser.getEmail()))
                 .password(Optional.ofNullable(newUser)
                         .map(UserModel::getPassword)
                         .orElse(oldUser.getPassword()))
+                .postIds(oldUser.getPostIds())
                 .build();
     }
 }
