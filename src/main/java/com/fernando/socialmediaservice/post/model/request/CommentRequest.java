305d5fe8f0b21cd1fffa46e0710f33ffbab114f1
frohr <frohr@grupodimed.com.br>
2021-11-28 11:01:08 -0300

    Adiciona swagger ao CommentRequest

--- src/main/java/com/fernando/socialmediaservice/post/model/request/CommentRequest.java
+++ src/main/java/com/fernando/socialmediaservice/post/model/request/CommentRequest.java
@@ -1,9 +1,12 @@
 package com.fernando.socialmediaservice.post.model.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
@@ -11,7 +14,13 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CommentRequest {
 
+    @NotBlank(message = "O nome do autor não pode ser vazio.")
+    @ApiModelProperty(value = "Nome do autor do comentário", example = "Alison", required = true)
     private String author;
+    @NotBlank(message = "O ID do autor não pode ser vazio.")
+    @ApiModelProperty(value = "ID do autor do comentário", example = "619ffd6ee93b7f4b3078e40a", required = true)
     private String authorId;
+    @NotBlank(message = "O comentário em si não pode ser vazio.")
+    @ApiModelProperty(value = "Comentário em si", example = "Muito bacana isso", required = true)
     private String body;
 }
